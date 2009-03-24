9fea4e1129f3415de5755238ac65d281f9cb7e1f
Renaud Chaput <renchap@cocoa-x.com>
2009-03-24 16:38:42 +0100

    Added achievement categories to api.get_characters_achievements
    achievements = api.get_characters_achievements
    categories = achievements.categories
    
    Next step : get details about a specific category, with full details about achievements.

--- lib/wowr/achievements.rb
+++ lib/wowr/achievements.rb
@@ -1,17 +1,52 @@
 module Wowr
 	module Classes
 	  class CharacterAchievementsInfos
-	    attr_reader :latest_achievements
+	    attr_reader :latest_achievements, :categories
 	    
 	    def initialize elem, api
 	      @api = api
 	      @latest_achievements = Array.new
+	      @categories = Array.new
 	      
 	      achievements = elem%'achievements'
-	      
 	      summary = achievements%'summary'
+	      
+	      # Get list of latest achievements
 	      summary.search('achievement').each do |achievement|
 	        @latest_achievements << CompletedAchievement.new(achievement)
+        end
+        
+        # Get the infos about categories completion
+        # They are ordered in same order as categories below
+        categories_completion = summary.search('category/c')
+
+        # Get the list of rootCategories
+        i = 0
+        achievements.search('rootCategories/category').each do |category|
+          elem = Hash.new
+          type = AchievementsCategoryDetails
+          elem['id'] = category['id']
+          elem['name'] = category['name']
+          completion = categories_completion[i]
+          elem['earned'] = completion['earned']
+          
+          # If we have more informations
+          if completion['total']
+            type = AchievementsCategoryDetailsWithPoints
+            elem['total'] = completion['total']
+            elem['totalPoints'] = completion['totalPoints']
+            elem['earnedPoints'] = completion['earnedPoints']
+          end
+          new_cat = type.new(elem)
+          
+          # Add subcategories
+          category.search('category').each do |subcategory|
+            subcat = AchievementsCategory.new subcategory
+            new_cat.add_subcategory subcat
+          end
+          
+          @categories << new_cat
+          i = i+1
         end
       end
     end 
@@ -38,6 +73,39 @@ module Wowr
 				rescue
 					@date_completed 	= achievement[:dateCompleted] == "" ? nil : achievement[:dateCompleted]
 				end
+      end
+    end
+    
+    class AchievementsCategory
+      attr_reader :name
+      attr_reader :subcategories
+      attr_writer :parent
+      def initialize category
+        @name = category['name']
+        @subcategories = Array.new
+      end
+      
+      def add_subcategory category
+        @subcategories << category
+        category.parent = self
+      end
+    end
+    
+    class AchievementsCategoryDetails < AchievementsCategory
+      attr_reader :earned
+      def initialize category
+        super(category)
+        @earned = category['earned'].to_i
+      end
+    end
+    
+    class AchievementsCategoryDetailsWithPoints < AchievementsCategoryDetails
+      attr_reader :earned_points, :total, :total_points
+      def initialize category
+        super(category)
+        @earned_points = category['earnedPoints'].to_i
+        @total = category['total'].to_i
+        @total_points = category['totalPoints'].to_i
       end
     end
   end
